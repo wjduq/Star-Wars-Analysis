6dd16a97e870124ce6f7b97614670c4fcacfc3a1
Riley Coburn <coburn_r1@denison.edu>
2021-10-21 16:57:21 -0400

    Update script-regex.py

--- script-regex.py
+++ script-regex.py
@@ -1,22 +1,18 @@
 import re 
 
 newHopePattern = r"(\b[A-Z ']+\b)\n[^\n]+[A-Z(]"
-
 newHopeScript = open('Star-Wars-A-New-Hope.txt', 'r')
-
 newHopeMatches = re.findall(newHopePattern, newHopeScript.read())
 print(newHopeMatches)
 
-empireStrikesBackPattern = r""
-
+empireStrikesBackPattern = r"(\b[A-Z ']+\b)\n[^\n]+[A-Z(]"
 empireStrikesBackScript = open('Star-Wars-The-Empire-Strikes-Back.txt','r')
-
 empireStrikesBackMatches = re.findall(empireStrikesBackPattern, empireStrikesBackScript.read())
 print(empireStrikesBackMatches)
 
-returnOfTheJediPattern = r""
-
-returnOfTheJediScript = open('Star-Wars-Return-of-the-Jedi.txt.','r')
-
+returnOfTheJediPattern = r"\n(\b[A-Z ']{3,}\b)\n"
+returnOfTheJediScript = open('Star-Wars-Return-of-the-Jedi.txt','r')
 returnOfTheJediMatches = re.findall(returnOfTheJediPattern, returnOfTheJediScript.read())
 print(returnOfTheJediMatches)
+
+returnOfTheJediScript.read()
