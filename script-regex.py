a2bb27cf4cdcb60162e1d45f888b624840f8fb6a
Riley Coburn <coburn_r1@denison.edu>
2021-10-21 16:49:59 -0400

    Update script-regex.py

--- script-regex.py
+++ script-regex.py
@@ -4,5 +4,19 @@ newHopePattern = r"(\b[A-Z ']+\b)\n[^\n]+[A-Z(]"
 
 newHopeScript = open('Star-Wars-A-New-Hope.txt', 'r')
 
-matches = re.findall(newHopePattern, newHopeScript.read())
-print(matches)
+newHopeMatches = re.findall(newHopePattern, newHopeScript.read())
+print(newHopeMatches)
+
+empireStrikesBackPattern = r""
+
+empireStrikesBackScript = open('Star-Wars-The-Empire-Strikes-Back.txt','r')
+
+empireStrikesBackMatches = re.findall(empireStrikesBackPattern, empireStrikesBackScript.read())
+print(empireStrikesBackMatches)
+
+returnOfTheJediPattern = r""
+
+returnOfTheJediScript = open('Star-Wars-Return-of-the-Jedi.txt.','r')
+
+returnOfTheJediMatches = re.findall(returnOfTheJediPattern, returnOfTheJediScript.read())
+print(returnOfTheJediMatches)
